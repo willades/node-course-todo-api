a125b4304eb05c8db9c92805e922ab14cec64f58
William Ades <adesw@bc.edu>
2017-10-21 11:37:46 -0400

    Added removebyid

--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get("/todos/:id", (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id))
+    return res.status(404).send();
+  Todo.findByIdAndRemove(id).then((doc) => {
+    if(!doc) {
+      return res.status(404).send();
+    }
+    res.send({ doc });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Started up at ${port}`);
 });
