aaa770bdb3f920e22a78c8300900ce283eee72f5
William Ades <adesw@bc.edu>
2017-10-22 12:19:37 -0400

    Added GET /users/me and added middlware

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,23 @@ UserSchema.methods.toJSON = function () {
   return _.pick(userObject, ['_id', 'name', 'email']);
 }
 
+UserSchema.statics.findByToken = function(token) { // statics means model method as opposed to instance method
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {ObjectID} = require('mongodb');
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -112,6 +113,12 @@ app.get('/users', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+  }, (e) => {
+    res.status(401).send(); // 401 means user is not authorized
+  });
 
 app.listen(port, () => {
   console.log(`Started up at ${port}`);
