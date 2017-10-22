09d7697a8a3fc4cdce07494b8c31ee4d6791edfc
William Ades <adesw@bc.edu>
2017-10-22 17:56:33 -0400

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,37 +1,53 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10,
-}
+var password = "123abc!";
 
-var token = jwt.sign(data, "123abc");
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.decode(token, "123abc");
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$a0fP7EJF7TJMFOJP6toGee9WMwZRc8qkBTbW34D4ik81SJIrdbFqa';
 
-var message = 'I am Will'
-var hash = SHA256(message).toString();
+bcrypt.compare('mypass', hashedPassword, (err, res) => {
+  console.log(res);
+})
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
 
-var data = {
-  id: 4,
-}
-
-var token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + 'secure').toString()
-
-}
-
-var resultHash = SHA256(JSON.stringify(token.data) + 'secure').toString()
-// token.hash = SHA256(JSON.stringify(token.data)).toString();
-
-if (resultHash === token.hash){
-  console.log('Data was not changed');
-} else {
-  console.log('Data changed. Do not trust.')
-}
+// var data = {
+//   id: 10,
+// }
+//
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
+//
+// var decoded = jwt.decode(token, "123abc");
+// console.log('decoded', decoded);
+//
+// var message = 'I am Will'
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// var data = {
+//   id: 4,
+// }
+//
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'secure').toString()
+//
+// }
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'secure').toString()
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+// if (resultHash === token.hash){
+//   console.log('Data was not changed');
+// } else {
+//   console.log('Data changed. Do not trust.')
+// }
